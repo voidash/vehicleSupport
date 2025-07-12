891f2d1e41e07adcdce1af740e87d23c3efbcfc8
--show <ashish.thapa477@gmail.com>
2025-07-13 01:28:40 +0545

    feat: minor changes on images

--- script.js
+++ script.js
@@ -6,37 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Rolls-Royce Phantom',
             type: 'Wedding & VIP',
             description: 'The pinnacle of automotive luxury. Perfect for making an unforgettable entrance on your wedding day.',
-            image: 'https://images.unsplash.com/photo-1629450646302-3d86c80fdd58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
+            image: 'https://hips.hearstapps.com/hmg-prod/images/phantom-scintilla-private-collection-0-1-66b50a5eddd44.jpg'
         },
         {
             name: 'Mercedes-Benz G-Class',
             type: 'Luxury Jeep Escort',
             description: 'A symbol of power and prestige. Command the city streets with this iconic luxury SUV.',
-            image: 'https://images.unsplash.com/photo-1603584071921-23724a2ac958?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            image: 'https://vehicle-images.dealerinspire.com/6ed8-11001591/thumbnails/large/W1NWM0AB2SX042655/51b1e05851452a315656bdb8f62591c7.jpg'
         },
         {
             name: 'Toyota Land Cruiser',
             type: 'Family & Temple Visits',
             description: 'Uncompromising safety and comfort for your family. A trusted companion for serene spiritual journeys.',
-            image: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            image: 'https://images.unsplash.com/photo-1650530579355-7ad9d4766043?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             name: 'Range Rover Vogue',
             type: 'Luxury Jeep Escort',
             description: 'The definitive luxury SUV. Experience peerless refinement and capability on any terrain.',
-            image: 'https://images.unsplash.com/photo-1580273916550-402b6a52091f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80'
+            image: 'https://hips.hearstapps.com/hmg-prod/images/2024-land-rover-range-rover-evoque-phev-102-65380162bded9.jpg'
         },
         {
             name: 'BMW 7 Series',
             type: 'Wedding & VIP',
             description: 'Combining dynamic presence with ultimate comfort. A statement of modern luxury for any special occasion.',
-            image: 'https://images.unsplash.com/photo-1592996237625-d8a775b27238?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
+            image: 'https://imgd.aeplcdn.com/664x374/n/cw/ec/132513/7-series-exterior-right-front-three-quarter-3.jpeg?isig=0&q=80'
         },
         {
             name: 'Ford Endeavour',
             type: 'Family & Temple Visits',
             description: 'A spacious and powerful SUV, offering a blend of ruggedness and comfort for family adventures.',
-            image: 'https://images.unsplash.com/photo-1617106315145-e3959984d16b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            image: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/Ford/Endeavour/11580/1710203310359/front-left-side-47.jpg'
         }
     ];
 
